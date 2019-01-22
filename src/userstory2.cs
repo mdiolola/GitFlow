ad29aade93e4ea2356ff14f3171fee5e3329295c
Mond Diolola <Mond.Diolola@coxautoinc.com>
2019-01-22 13:08:15 +0800

    sp2 update3

--- src/userstory2.cs
+++ src/userstory2.cs
@@ -7,5 +7,6 @@ namespace gitflow
         public string update1 { get; set; }
         public string update2 { get; set; }
         public string sp2update2 { get; set; }
+        public string sp2update3 { get; set; }
     }
 }
