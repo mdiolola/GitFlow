0268246e8dc4f4afbba182c20ea27e3fde896a35
Mond Diolola <Mond.Diolola@coxautoinc.com>
2019-01-22 13:11:52 +0800

    sp3 update1

--- src/userstory1.cs
+++ src/userstory1.cs
@@ -5,16 +5,19 @@ namespace gitflow
         public string hotfix { get; set; }
         public string hotfix2 { get; set; }
         public string release2 { get; set; }
+        public string sp3update1 { get; set; }
         public string update1 { get; set; }
         public string update2 { get; set; }
         public int fine { get; set; }
         public string sp2update1 { get; set; }
         public string sp2update2 { get; set; }
         public string sp2update3 { get; set; }
+
     }
     public class sprint
     {
         public string sprinting { get; set; }
+        public string sp3update1 { get; set; }
         public string sp2update2 { get; set; }
 
     }
--- src/userstory2.cs
+++ src/userstory2.cs
@@ -3,6 +3,7 @@ namespace gitflow
     public class userstory2
     {
         public string Error { get; set; }
+        public string sp3update1 { get; set; }
         public string NoError { get; set; }
         public string update1 { get; set; }
         public string update2 { get; set; }
