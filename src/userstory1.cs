9e7b17e84d79a8a7f9fcfef5d05903ab855e9440
Mond Diolola <Mond.Diolola@coxautoinc.com>
2019-01-22 13:21:03 +0800

    remove hotfix

--- src/userstory1.cs
+++ src/userstory1.cs
@@ -2,8 +2,6 @@ namespace gitflow
 {
     public class UserStory1
     {
-        public string hotfix { get; set; }
-        public string hotfix2 { get; set; }
         public string release2 { get; set; }
         public string sp3update1 { get; set; }
         public string update1 { get; set; }
