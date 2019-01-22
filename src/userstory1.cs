4b4fe78dbe840aa771011c9b23643e0a5b0b1fa3
Mond Diolola <Mond.Diolola@coxautoinc.com>
2019-01-22 12:37:07 +0800

    sprinting

--- src/userstory1.cs
+++ src/userstory1.cs
@@ -3,5 +3,10 @@ namespace gitflow
     public class UserStory1
     {
         public string hotfix { get; set; }
+        public int fine { get; set; }
+    }
+    public class sprint
+    {
+        public string sprinting { get; set; }
     }
 }
